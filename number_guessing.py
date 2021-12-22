0c9624606b0af0fa827685b16b2942022ee61692
realryankim <ryankimofficial@gmail.com>
2021-12-22 22:41:56 +0900

    add random_number_guessing_game

---
+++ number_guessing.py
@@ -0,0 +1,33 @@
+# random number guessing game
+
+import random
+
+while True: # play forever
+    flag = True
+    while flag:
+        num = input("Type a number for an upper bound: ")
+
+        if num.isdigit():
+            print("Let's play!")
+            num = int(num)
+            flag = False
+        else:
+            print("Invalid input! Try Again")
+
+    secret = random.randint(1, num)
+
+    guess = None
+    count = 1
+
+    while guess != secret:
+        guess = input("Please type a number between 1 and " + str(num) + ": ")
+        if guess.isdigit(): # to check guess is digit
+            guess = int(guess)
+
+        if guess == secret:
+            print("You got it!")
+        else:
+            print("Please try again")
+            count += 1
+
+    print('It took you', count, 'guesses!')
